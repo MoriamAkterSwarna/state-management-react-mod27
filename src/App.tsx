42ff2e436cc7bb5808c58926628782a0110d4b84
Moriam Akter Swarna <moriam15-12880@diu.edu.bd>
2026-08-19 17:52:21 +0600

    Code for video-2 added

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import "./App.css";
+
+function App() {
+  const handleClick2 = () => {
+    console.log("Clicked! 2");
+  };
+  function handleClick3() {
+    console.log("Clicked! 3 ");
+  }
+
+  const handleDelete = (id: number) => {
+    console.log(`Delete item ${id}`);
+  };
+
+  return (
+    <>
+      {/* Way 1 */}
+      <button onClick={() => console.log("Clicked! 1")}>Click me1</button>
+
+      {/* Way 2  with arrow function */}
+      <button onClick={handleClick2}>Click me 2</button>
+
+      {/* way 2 with normal function */}
+      <button onClick={handleClick3}>Click me 3</button>
+
+      {/* Way 3 with parameter */}
+      <button onClick={() => handleDelete(123)}>Delete</button>
+    </>
+  );
+}
+
+export default App;
